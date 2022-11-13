b1fec27df94f87c93ed8ad671ee67aa64ed229a4
lemonadern <taishi.develop@gmail.com>
2022-11-14 05:30:00 +0900

    wip

--- parser.ts
+++ parser.ts
@@ -23,6 +23,25 @@ type AST = {
 export function parser(tokens: Token[]) {
   let i = 0;
 
+  function parseBlock(): Node {
+    let token = tokens[i];
+
+    if (token.kind === "curlyBracket" && token.value === "{") {
+      token = tokens[++i];
+      const node: ExpressionNode = {
+        kind: "block",
+        value: token.value,
+        children: [],
+      };
+      token = tokens[++i];
+
+      while ((token.kind === "curlyBracket")) {
+      }
+    } else {
+      throw Error(`Block must be started by "{", but got ${token.value}`);
+    }
+  }
+
   function parse(): Node {
     let token = tokens[i];
 
